c6a6386747963a9211e04833c3c2280785e516b0
nahco314 <nahco3_ta@yahoo.co.jp>
2022-08-14 11:40:28 +0900

    コマンドで変換できるようにした

--- compatibilityer/convert.py
+++ compatibilityer/convert.py
@@ -3,10 +3,11 @@ from typing import Type
 from compatibilityer.converter import Converter
 
 import ast
+from pathlib import Path
 
 
 HEAD = """\
-@converted by compatibilityer.convert.convert
+# @converted by compatibilityer.convert.convert
 
 """
 
@@ -22,3 +23,13 @@ def convert(code: str, converter: Type[Converter] = Converter) -> str:
 
 def convert_file(code: str, converter: Type[Converter] = Converter, head: str = HEAD) -> str:
     return head + convert(code, converter)
+
+
+def convert_dir(dir_: Path, converter: Type[Converter] = Converter, head: str = HEAD) -> None:
+    assert dir_.is_dir()
+    for file in dir_.glob('**/*.py'):
+        with open(file, "r") as f:
+            c = f.read()
+        with open(file, "w") as f:
+            nc = convert_file(c, converter, head)
+            f.write(nc)
